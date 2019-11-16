855e5f8ae2d04c0aff0ee25756f026260de46240
Vyacheslav Mishukov <ustmish@yandex.ru>
2019-11-16 16:58:44 +0300

    docker-2 20191116 01

---
+++ docker-monolith/infra/terraform/modules/app/outputs.tf
@@ -0,0 +1,12 @@
+#output "app_external_ip1" {
+#value = google_compute_instance.app[0].network_interface.0.access_config.0.nat_ip
+#}
+#output "app_external_ip2" {
+#value = google_compute_instance.app[1].network_interface.0.access_config.0.nat_ip
+#}
+#output "app_external_ip3" {
+#value = google_compute_instance.app[2].network_interface.0.access_config.0.nat_ip
+#}
+#output "app_external_ip4" {
+#value = google_compute_instance.app[3].network_interface.0.access_config.0.nat_ip
+#}
---
+++ docker-monolith/infra/terraform/stage/variables.tf
@@ -0,0 +1,63 @@
+variable project {
+  description = "Project ID"
+}
+variable region {
+  description = "Region"
+  # Значение по умолчанию
+  default = "europe-west2"
+}
+
+variable zone {
+  description = "Region Instance"
+  default     = "europe-west2-b"
+}
+
+variable public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+
+variable private_key_path {
+  # Описание переменной
+  description = "Path to the private key used for ssh access"
+}
+
+variable disk_image {
+  description = "Disk image"
+  default = "ubuntu-1604-xenial-v20191113"
+}
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default = "ubuntu-1604-xenial-v20191113"
+}
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default = "reddit-db-base"
+}
+
+variable "instance_count" {
+  default = "4"
+}
+
+variable app_name{
+  description = "application name"
+  default = "reddit-app"
+}
+variable ip_name{
+  description = "name of ip resource"
+}
+variable firewall_puma_name{
+  description = "firewall puma name"
+}
+
+variable instance_db_name{
+  description = "name of instance"
+}
+variable firewall_mongo_name{
+  description = "firewall_mongo_name"
+}
+variable ssh_name{
+  description = "firewall name for ssh access"
+}
+
