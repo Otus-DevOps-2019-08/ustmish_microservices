ad996ceb0ef01bd893539d053b605a2c2335309a
Vyacheslav Mishukov <ustmish@yandex.ru>
2019-12-13 01:23:24 +0300

    monitoring-1 20191213 01

--- docker-monolith/infra/packer/scripts/install_ruby.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-
-sudo apt update 
-sudo apt install -y ruby-full ruby-bundler build-essential
-
-
-
--- docker-monolith/infra/terraform/stage/outputs.tf
+++
@@ -1,3 +0,0 @@
-#output "app_external_ip1" {
-#  value = module.app.app_external_ip1
-#}
